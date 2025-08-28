b1a57327ebf1e58fcf96b2e1d3dc2e7a4fd6a0b9
ST-0301 <stng0301@gmail.com>
2025-08-29 05:28:27 +0800

    Updates

---
+++ functions/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    requireConfigFile: false,
+  },
+  extends: ["eslint:recommended", "google"],
+  rules: {
+    "no-restricted-globals": ["error", "name", "length"],
+    "prefer-arrow-callback": "error",
+    "quotes": ["error", "double", {allowTemplateLiterals: true}],
+    "max-len": [
+      "error",
+      {
+        code: 120,
+        ignoreComments: true,
+        ignoreStrings: true,
+        ignoreUrls: true,
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["**/*.spec.*"],
+      env: {
+        mocha: true,
+      },
+      rules: {},
+    },
+  ],
+  globals: {},
+};
